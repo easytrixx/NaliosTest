b9ca1229b2edb808166267cc11fb6ebac6a72d16
easytrixx <clement.massee@gmail.com>
2022-09-08 14:01:06 +0200

    update ex03

--- ex03.js
+++ ex03.js
@@ -1,5 +1,5 @@
 
 
-const helloNalios=['h','e','l','l','o',' ','N','a','l','i','o','s']
-console.log(helloNalios.join(''))
+console.log(String.fromCharCode(72, 101,108,108,111,32,78,97,108,105,111,115));
+
 
